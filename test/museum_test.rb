1f6650ae86c24b4cd5c2f8ef7bb2e59585ba60c6
Connor Ferguson <cpfergus1@gmail.com>
2020-09-10 10:28:09 -0600

    Added test for lottery contestants

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -75,7 +75,18 @@ class MuseumTest <Minitest::Test
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
 
-  
+  def test_ticket_lottery_contestants
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 5)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expected = [patron_2, patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+  end
 
 
 
