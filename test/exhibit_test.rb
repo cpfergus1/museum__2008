aa8b91ef20f37747571fa5f1bcb1910f450deff9
Connor Ferguson <cpfergus1@gmail.com>
2020-09-10 09:09:58 -0600

    Added test for exhibit attributes

---
+++ test/exhibit_test.rb
@@ -0,0 +1,11 @@
+require './lib/exhibit'
+require './lib/patron'
+
+class ExhibitTest <Minitest::Test
+
+  def test_exhibit_has_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+end
